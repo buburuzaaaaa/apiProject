30850dbe8177b46b87b91b0c0d267305ce605fae
Sarc4sticNerd <jplam904@gmail.com>
2023-10-31 21:24:08 -0700

    quotes

--- public/quotes.js
+++ public/quotes.js
@@ -2,5 +2,5 @@ async function quote() {
   let quote = await axios.get('/api/quotes/getQuote');
   console.log(quote);
   document.getElementsByTagName('p')[0].innerHTML =
-    quote.data[0].quote + '-' + quote.data[0].author;
+    quote.data[0].quote + ' - ' + quote.data[0].author;
 }
